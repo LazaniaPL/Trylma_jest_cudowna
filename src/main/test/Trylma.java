91120dd7e549cc35ca98e2b97cb32004bce73fb7
LazaniaPL <rysiu.piwowarski@onet.eu>
2018-12-04 22:59:41 +0100

    Kurwa Kogoś pojebało Tworzenie trylmy działa zajebiście (jeszcze gridpane)

--- src/main/test/Trylma.java
+++ src/main/test/Trylma.java
@@ -9,18 +9,47 @@ class Trylma extends TrylmaBuilder {
 
 
     @Test
-    void CountOfTrylmaObjects() {
+    void sizeOfTrylma() {
         //szerokość to 4 * scale + 1
         //długość to 6 * scale + 1
 
-        TrylmaBuilder trylmaBuilder = new TrylmaBuilder();
-        trylmaBuilder.scale = 6;
+        TrylmaBuilder trylmaBuilderTest = new TrylmaBuilder();
+        trylmaBuilderTest.scale = 6;
         GridPane test = new GridPane();
-        trylmaBuilder.setTrylma(test);
-        trylmaBuilder.whatIsObject();
-        assertEquals(4 * trylmaBuilder.scale + 1, test.impl_getColumnCount()); //szerokosc
-        assertEquals(6 * trylmaBuilder.scale + 1, test.impl_getRowCount()); //dlugosc
+        trylmaBuilderTest.setTrylma(test);
+        trylmaBuilderTest.whatIsObject();
+        assertEquals(4 * trylmaBuilderTest.scale + 1, test.impl_getColumnCount()); //szerokosc
+        assertEquals(6 * trylmaBuilderTest.scale + 1, test.impl_getRowCount()); //dlugosc
 
     }
 
+
+    @Test
+    void howMuchObjects() {
+
+
+        TrylmaBuilder trylmaBuilderTest = new TrylmaBuilder();
+        trylmaBuilderTest.scale = 77;
+
+        GridPane test = new GridPane();
+        trylmaBuilderTest.setTrylma(test);
+        trylmaBuilderTest.whatIsObject();
+        assertEquals(numbersOfObjects(trylmaBuilderTest.scale), trylmaBuilderTest.numberOfObjects);
+
+
+    }
+
+
+    int numbersOfObjects(int scale) {
+        int a = 0, b = 0;
+        for (int k = 0; k <= scale * 3 + 1; k++) {
+            a = a + k;
+        }
+        for (int k = 0; k <= scale; k++) {
+            b = b + k;
+
+        }
+
+        return a + 3 * b;
+    }
 }
