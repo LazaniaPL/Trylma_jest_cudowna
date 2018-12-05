22a2e00b47cc903381654290e81b8b7f406bfd60
LazaniaPL <rysiu.piwowarski@onet.eu>
2018-12-05 21:37:08 +0100

    Wszystko działa ale jeszcze nie obrócone

---
+++ src/main/java/Trylma/players/PlayerD.java
@@ -0,0 +1,19 @@
+package Trylma.players;
+
+public class PlayerD implements Player {
+
+
+    public PlayerD(int[][] trylma, int scale) {
+        for (int szerokosc = 0; szerokosc < scale; szerokosc++) {
+
+            for (int tempWysokosc = 0; tempWysokosc <= szerokosc; tempWysokosc++) {
+
+                //prawy trójkąt
+                trylma[4 * scale - szerokosc][
+                        3 * scale - szerokosc + 2 * tempWysokosc] = 5;
+
+
+            }
+        }
+    }
+}
